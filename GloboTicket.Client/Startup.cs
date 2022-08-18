800b0cececc440851ea8c373122031fd39e472d4
Siddharth Shailesh Mehta <mehta.siddharth87@gmail.com>
2022-08-18 16:57:48 +0530

    Client OpenID connect authentication.

--- GloboTicket.Client/Startup.cs
+++ GloboTicket.Client/Startup.cs
@@ -1,6 +1,8 @@
 using Dapr.Client;
 using GloboTicket.Web.Models;
 using GloboTicket.Web.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -35,7 +37,7 @@ namespace GloboTicket.Web
             {
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
                 PropertyNameCaseInsensitive = true,
-            });
+            });            
 
             services.AddSingleton<IEventCatalogService>(c =>
                 new EventCatalogService(DaprClient.CreateInvokeHttpClient("catalog")));
@@ -46,6 +48,29 @@ namespace GloboTicket.Web
             services.AddScoped<IDiscountService, DiscountDaprService>();            
 
             services.AddSingleton<Settings>();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            }).AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+            {
+                options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.Authority = "https://login.microsoftonline.com/{guid}/v2.0";
+                options.ClientId = "";
+
+                //Web apps that call web APIs are confidential client applications.
+                //That's why they register a secret (an application password or certificate) with Azure Active Directory (Azure AD).
+                //This secret is passed in during the call to Azure AD to get a token.
+                options.ClientSecret = "";
+                options.ResponseType = "code";
+
+                //options.ResponseType = "id_token"; // only for web app that is used to signs in user. Also we need to check ID Tokens in AAD registration page
+                options.Scope.Add("api://catalog/FullAccess");
+                options.Scope.Add("offline_access"); // to get refresh tokens
+                options.SaveTokens = true;
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -64,6 +89,8 @@ namespace GloboTicket.Web
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
